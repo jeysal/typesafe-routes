318954c2e6d598c5788111edea39ced5a1854349
Denis Kruschinski <denis.kruschinski@gmail.com>
2020-10-03 12:06:55 +0200

    typo

--- src/demo/express.ts
+++ src/demo/express.ts
@@ -20,7 +20,7 @@ const mainView = (p?: {
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>typesafe-router express demo</title>
+    <title>typesafe-routes express demo</title>
   </head>
   <body>
     <h2>Accounts</h2>
